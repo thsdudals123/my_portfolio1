51248a98721d38741e6df3290dbc95bfa0b2f4f0
USER <USER@test.com>
2018-04-04 10:35:06 +0900

    create my_portfolio1

---
+++ my_portfolio1/src/my_portfolio1/my_portfolio1.java
@@ -0,0 +1,10 @@
+package my_portfolio1;
+
+public class my_portfolio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Created Project");
+	}
+
+}
